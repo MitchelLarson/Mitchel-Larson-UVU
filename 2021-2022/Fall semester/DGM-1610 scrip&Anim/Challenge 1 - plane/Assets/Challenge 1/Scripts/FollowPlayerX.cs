9e3df7fa90751264b6ad3b8491b45bcd31cacae4
Mitchel Larson <mitchel.larson@hotmail.com>
2021-10-16 09:47:46 -0600

    Plane Challenge

---
+++ 2021-2022/Fall semester/DGM-1610 scrip&Anim/Challenge 1 - plane/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject playerX;
+    private Vector3 offset = new Vector3(20, 2, 10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("started");
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Debug.Log("lateupdatestarted");
+        transform.position = playerX.transform.position + offset;
+    }
+}
