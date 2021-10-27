80beee372cf78e327d4b4d62d5713887f09c48eb
Mitchel Larson <mitchel.larson@hotmail.com>
2021-10-27 14:04:35 -0600

    play fetch

---
+++ 2021-2022/Fall semester/DGM-1610 scrip&Anim/Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float DogSpawnTime = 3.0f;
+    private float timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timer > DogSpawnTime)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timer = 0;
+        }
+    }
+}
