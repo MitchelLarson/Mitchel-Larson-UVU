3d1a59d1fa57f5b1d7f702eabe2a2b490e2da801
Mitchel Larson <mitchel.larson@hotmail.com>
2021-11-10 22:54:32 -0700

    Challenge 4

--- 2021-2022/Fall semester/DGM-1610 scrip&Anim/Gameplay Mechanics/Assets/Scripts/enemy.cs
+++ 2021-2022/Fall semester/DGM-1610 scrip&Anim/Gameplay Mechanics/Assets/Scripts/enemy.cs
@@ -21,7 +21,7 @@ public class enemy : MonoBehaviour
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         
-        enemyRb.AddForce(lookDirection* speed);
+        enemyRb.AddForce(lookDirection * speed);
 
         if (transform.position.y < -10)
         {
